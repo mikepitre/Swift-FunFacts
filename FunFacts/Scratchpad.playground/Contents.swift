e1f1e7a82df2a778dc12d7efb5fd9f9b9407ba94
Mike Pitre <michaelepitre@gmail.com>
2015-09-14 14:53:46 -0500

    initial commit

---
+++ FunFacts/Scratchpad.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+//Optionals
+
+var optionalString: String? = "hello"
+optionalString = nil
+
+var randomNumber = Int(arc4random_uniform(10))
+
+
+//UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1)
+
+let colorsArray = []
+
+
+
+
+
+
+
+
